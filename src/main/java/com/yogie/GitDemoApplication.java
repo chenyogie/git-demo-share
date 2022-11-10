7898c789c6f33decf91d534a05885ceb5b198d8a
yogie <chenyogie@163.com>
2022-11-10 14:20:59 +0800

    first commit

---
+++ src/main/java/com/yogie/GitDemoApplication.java
@@ -0,0 +1,16 @@
+package com.yogie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yogie
+ */
+@SpringBootApplication
+public class GitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yogie/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
