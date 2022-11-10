dc03dba966d5e8eeb23ff039789b85b5ab61d53d
yogie <chenyogie@163.com>
2022-11-10 14:23:56 +0800

    second commit

--- src/main/java/com/yogie/GitDemoApplication.java
+++ src/main/java/com/yogie/GitDemoApplication.java
@@ -11,6 +11,7 @@ public class GitDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
+        System.out.println("second commit");
     }
 
 }
